23d1bb06013cb1951a7a7f0638e2ec2c4bd08522
Mammet-tech <edwinmammet19@gmail.com>
2025-03-15 23:54:36 +0300

    Add js events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (taskInput.value.trim() === "") {
+      alert("You must enter a task!");
+      return;
+    }
+    const task = document.createElement("li");
+    task.innerText = taskInput.value;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.addEventListener("click", function () {
+      task.remove();
+    });
+    task.appendChild(deleteButton);
+    taskList.appendChild(task);
+    taskInput.value = "";
+  });
 });
